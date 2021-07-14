84126d5c32f83fe99f4c0bde71830411399a5c30
Omkar1234-gif <78145618+Omkar1234-gif@users.noreply.github.com>
2021-07-14 08:11:20 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+function addUser()
+{
+player1_name = document.getElementById("Player1").value;                                
+player2_name = document.getElementById("Player2").value;
+localStorage.setItem("player1" , player1_name);
+localStorage.setItem("player2" , player2_name);
+window.location = "game_page.html";
+}
